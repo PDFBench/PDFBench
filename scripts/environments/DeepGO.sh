95ef546e084e6b059a7aa44e4fde68a203aefef8
Knlife <2512955763@qq.com>
2025-09-19 09:45:02 +0800

    feat: move env to /scripts/environment

---
+++ scripts/environments/DeepGO.sh
@@ -0,0 +1,4 @@
+pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2
+pip install dgl==1.1.2+cu117 -f https://data.dgl.ai/wheels/cu117/repo.html
+pip install pyyaml
+pip install simple_parsing
---
+++ scripts/environments/PDF.sh
@@ -0,0 +1,13 @@
+# PDFBench requirement
+conda install pytorch::faiss-gpu=1.8.0 --yes
+pip install torch
+pip install transformers
+pip install accelerate
+pip install esm
+pip install simple_parsing
+# requirements for sub-module [ProTrek] 
+pip install httpx
+pip install torchmetrics==0.9.3
+pip install pytorch-lightning==2.1.3
+# requirements for sub-module [EvoLlama]
+pip install peft
